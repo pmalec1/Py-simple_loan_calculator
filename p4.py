8559b263d5a924f9de094c136606a55f04a75ebb
pmalec1 <01169056@pw.edu.pl>
2023-01-05 12:32:29 +0100

    functions that calculate each part of installment

--- p4.py
+++ p4.py
@@ -16,3 +16,18 @@ def get_inputs_from_user():
     credit_informations = {'Credit amount': credit_amount, 'Interest rate': interest_rate,
                            'Banks fee': banks_fee, 'Payment period': payment_period}
     return credit_informations
+
+
+def calculate_interest_intallment(total_credit_amount, number_of_month, capital_installment, provision_per_year):
+    interest_installment = ((total_credit_amount-number_of_month*capital_installment)*provision_per_year)/12
+    return round(interest_installment, 2)
+
+
+def calculate_capital_installment(total_credit_amount, amount_of_months):
+    capital_installment = total_credit_amount / amount_of_months
+    return round(capital_installment, 2)
+
+
+def calculate_total_installment(interest_intallment, capital_installment):
+    total_installment = interest_intallment+capital_installment
+    return round(total_installment, 2)
